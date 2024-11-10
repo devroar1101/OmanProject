eb501c91a141ca9ae74aa54bb48e50651b326ac6
devroar1101 <devroar1101@gmail.com>
2024-11-10 12:32:03 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:tenderboard/admin/listmaster/screens/listmaster_home.dart';
-import 'package:tenderboard/common/screens/home.dart';
 import 'package:tenderboard/common/screens/login.dart';
-//import 'package:tenderboard/admin/listmasteritem/screens/listmasteritem_home.dart';
-// Import the ListMasterItemScreen
 
 void main() {
   runApp(MyApp());
