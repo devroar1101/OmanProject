bda8208f8e4a918569a196fe6764e32c962c8e3d
AlwinAntony03 <alwinantony1208@gmail.com>
2025-04-21 15:04:06 +0530

    letter

--- lib/office/letter/screens/letter_form.dart
+++ lib/office/letter/screens/letter_form.dart
@@ -1086,7 +1086,7 @@ class _LetterFormState extends ConsumerState<LetterForm> {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Column(
-        spacing: 8,
+       // spacing: 8,
         children: [
           Row(children: [
             Expanded(
